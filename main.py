c18bf85f3ce2a43e3212121730885dffec11a837
Alex Jilkin <jilkinalex@gmail.com>
2023-06-14 09:46:08 +0300

    Improve code and include sample files

--- main.py
+++ main.py
@@ -3,44 +3,46 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import hashlib
+import time
 
 memo = {}
+
+total_clique_tree = 0
+total_hash = 0
+
 def count(G: nx.Graph):
-    # G_hash = nx.graph_hashing.weisfeiler_lehman_graph_hash(G)
+    global total_clique_tree, total_hash
+
+    start = time.process_time()
 
     hashable_graph = tuple(sorted(G.nodes.items()) + sorted(G.edges.items()))
     G_hash = hashlib.sha256(str(hashable_graph).encode()).hexdigest()
-
+    total_hash += time.process_time() - start
     try:
         res = memo[G_hash]
         return res
     except KeyError:
         pass
     
-    clique_tree = nx.Graph()
+    print(f"Hash - {time.process_time() - start}")
+
+    clique_tree = nx.junction_tree(G)
+    # clique_tree = maximal_clique_tree(G)
+    
     visited = []
     
-    # Filter out cliques that are subsets of others
-    maximal_cliques = list(map(lambda clique: tuple(sorted(clique)), nx.find_cliques(G)))
-    clique_tree.add_nodes_from(maximal_cliques)
+    # Sort maximal cliques members to acsending tuples
+    # maximal_cliques = list(map(lambda clique: tuple(sorted(clique)), nx.find_cliques(G)))
+    maximal_cliques = list(clique_tree.nodes)
 
-    # Builds graph out of maximal cliques by content intersection
-    for clique1 in maximal_cliques:
-        for clique2 in maximal_cliques:
-            if (clique1 != clique2 and len(set(clique1).intersection(clique2)) > 0):
-                if(len([edge for edge in clique_tree.edges() if clique1 in edge]) == 0):
-                    clique_tree.add_edge(clique1, clique2)
-                    break
-
-    r = maximal_cliques[0]
-    # print(f"r={r}")
+    total_clique_tree += (time.process_time() - start)
+    r = list(clique_tree.nodes)[0]
     sum = 0
 
     Q = [r]
     visited = [r]
 
-    # nx.draw_networkx(clique_tree)
-    # plt.show()
+    # print(f"Finding junction tree - {time.process_time() - start}")
     while len(Q) > 0:
         v = Q.pop(0)
         
@@ -70,7 +72,12 @@ def count(G: nx.Graph):
     
     return sum
 
+total_FP = 0
+
 def FP(T, r, v):
+    global total_FP
+
+    start = time.process_time()
     res = []
 
     path = list(nx.shortest_path(T, r, v))
@@ -83,6 +90,8 @@ def FP(T, r, v):
     
     # Converted to set for uniqness and sorted
     res = set(tuple(sorted(s)) for s in res)
+
+    total_FP += (time.process_time() - start)
     
     return list(res)
 
@@ -101,7 +110,11 @@ def fac(n):
     fmemo[n] = res
     return res
 
+total_phi = 0
 def phi(cliquesize, i, fp, pmemo):
+    global total_phi
+
+    start = time.process_time()
     if i in pmemo.keys():
         return pmemo[i]
     
@@ -109,10 +122,33 @@ def phi(cliquesize, i, fp, pmemo):
     for j in range (i+1, len(fp)):
         sum -= fac(fp[j]-fp[i]) * phi(cliquesize, j, fp, pmemo)
     pmemo[i] = sum
+    total_phi += (time.process_time() - start)
     return sum
+
+
+def maximal_clique_tree(G: nx.Graph):
+    clique_tree = nx.Graph()
+    maximal_cliques = list(map(lambda clique: tuple(sorted(clique)), nx.find_cliques(G)))
+
+    clique_tree.add_nodes_from(maximal_cliques)
+
+    # Builds graph out of maximal cliques by content intersection
+    for clique1 in maximal_cliques:
+        for clique2 in maximal_cliques:
+            if (clique1 != clique2 and len(set(clique1).intersection(clique2)) > 0):
+                # if(len([edge for edge in clique_tree.edges() if clique1 in edge]) == 0):
+                clique_tree.add_edge(clique1, clique2)
+
+    clique_tree = nx.maximum_spanning_tree(clique_tree, algorithm="kruskal")
+    return clique_tree
+
+total_C = 0
 
 # C_G(K) - algorithm 4
 def C(G: nx.Graph, K: set):
+    global total_C
+
+    start = time.process_time()
     S = [K, set(G.nodes) - K]
 
     to = []
@@ -121,7 +157,6 @@ def C(G: nx.Graph, K: set):
     
     while len(S) != 0:
         X = list(filter(lambda s: len(s) != 0, S))[0]
-        # v = list(X)[0]
         v = random.choice(list(X))
         to.append(v)
 
@@ -143,12 +178,15 @@ def C(G: nx.Graph, K: set):
         
         S = list(filter(lambda S_new_i: len(S_new_i) > 0, S_new))
     
+    total_C += (time.process_time() - start)
     return output
 
 
 def from_file():
+    start = time.process_time()
+
     # file = open('./sample.gr', 'r')
-    file = open('./interval-n=512-nr=1.gr', 'r')
+    file = open('./interval-n=1024-nr=1.gr', 'r')
     G = nx.Graph()
     lines = [tuple(map(int, line.strip().split(" "))) for line in file.readlines()]
     nodes_count = lines[0][0]
@@ -157,7 +195,11 @@ def from_file():
     nodes = np.arange(1, nodes_count + 1)
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
+    print(f"Read graph - {time.process_time() - start}")
+    start = time.process_time()
     print(f"#AMO={count(G)}")
+
+    print(f"#AMO - {time.process_time() - start}")
         
 # G = nx.Graph()
 
@@ -167,3 +209,5 @@ def from_file():
 # print(f"#AMO={count(G)}")
 
 from_file()
+
+print(f"total_C={total_C} \n total_FP={total_FP} \n total_phi={total_phi} \n total__clique_tree={total_clique_tree} \n total_hash={total_hash}")
