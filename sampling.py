688d1467db32ce72228c5ef17d2b05d99c3a808e
Alex Jilkin <jilkinalex@gmail.com>
2023-06-26 09:03:30 +0300

    More work on sampler and add data

--- sampling.py
+++ sampling.py
@@ -1,16 +1,35 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 from utils import read_graph_from_file
 
 def main():
-    G = read_graph_from_file('sample.gr')
-    sample(G)
+    G = read_graph_from_file('sample.gr', True)
+    d_G = nx.DiGraph(G)
+    sample(d_G)
 
 # G is a UCCG
-def sample(G: nx.Graph):
-    d_G = nx.DiGraph(G)
+def sample(G: nx.DiGraph):
+    
 
-    nx.draw_networkx(d_G, arrows=True)
-    plt.show()
+    # nx.draw_networkx(d_G, arrows=True)
+    # labels = nx.get_edge_attributes(d_G,'weight')
+    # pos = nx.spring_layout(d_G)
+    # nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+    # plt.show()
 
+    print(score(G))
+
+
+
+def score(G: nx.DiGraph):
+    score = 1
+    def local_score(node):
+        parents_score = list(map(lambda in_edge: G.get_edge_data(*in_edge)['weight'], G.in_edges(node)))
+        return np.array(parents_score).sum()
+    
+    for node in G.nodes:
+        score = score * local_score(node)
+        
+    return np.abs(score)
 main()
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import numpy as np
-def read_graph_from_file(filename):
+def read_graph_from_file(filename, random_weights=False):
 
     # file = open('./sample.gr', 'r')
     file = open(filename, 'r')
@@ -11,6 +11,10 @@ def read_graph_from_file(filename):
     
     nodes = np.arange(1, nodes_count + 1)
     G.add_nodes_from(nodes)
+    for edge in edges:
+        weight = np.round(np.random.normal(), 2) if random_weights else 1
+
+        G.add_edge(edge[0], edge[1], weight=weight)
     G.add_edges_from(edges)
 
     return G
