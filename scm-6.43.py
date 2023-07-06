deb78584590ca60c801974d9df36e502c0e7e0b4
Alex Jilkin <jilkinalex@gmail.com>
2023-07-06 14:21:56 +0300

    Plot it as well

--- scm-6.43.py
+++ scm-6.43.py
@@ -1,5 +1,7 @@
 import numpy as np
 import statsmodels.api as sm
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
 
 # The lm() function creates a linear regression model in R. 
 # This function takes an R formula Y ~ X where Y is the 
@@ -15,13 +17,13 @@ rnorm = np.random.normal(size=n)
 
 C = rnorm
 A = 0.8*np.random.normal(size=n)
-K = A + 0.1 * np.random.normal(size=n)
-X = C - 2*A + 0.2 * np.random.normal(size=n)
-F = 3*X + 0.8 * np.random.normal(size=n)
-D = -2*X + 0.5* np.random.normal(size=n)
-G = D + 0.5 * np.random.normal(size=n)
-Y = 2*K - D + 0.2 * np.random.normal(size=n)
-H = 0.5*Y + 0.1 * np.random.normal(size=n)
+K = A + 0.1*np.random.normal(size=n)
+X = C - 2*A + 0.2*np.random.normal(size=n)
+F = 3*X + 0.8*np.random.normal(size=n)
+D = -2*X + 0.5*np.random.normal(size=n)
+G = D + 0.5*np.random.normal(size=n)
+Y = 2*K - D + 0.2*np.random.normal(size=n)
+H = 0.5*Y + 0.1*np.random.normal(size=n)
 
 # Linear regression
 X1 = sm.add_constant(X)
@@ -38,3 +40,17 @@ X3 = sm.add_constant(np.column_stack((X, F, C, K)))
 model3 = sm.OLS(Y, X3)
 results3 = model3.fit()
 print(results3.params)
+
+
+# Generate x values for plotting
+x_vals = np.linspace(np.min(X), np.max(X), 100)
+
+# Plot the functions
+plt.scatter(X, Y, label='Data')
+plt.plot(x_vals, results1.params[0] + results1.params[1] * x_vals, label='Y ~ X')
+plt.plot(x_vals, results2.params[0] + results2.params[1] * x_vals + results2.params[2] * K, label='Y ~ X + K')
+plt.plot(x_vals, results3.params[0] + results3.params[1] * x_vals + results3.params[2] * F + results3.params[3] * C + results3.params[4] * K, label='Y ~ X + F + C + K')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.show()
