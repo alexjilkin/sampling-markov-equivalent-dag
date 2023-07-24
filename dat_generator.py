4f72d3781ab20b838cdc8aa6dbbaffc16be197a9
Alex Jilkin <jilkinalex@gmail.com>
2023-07-24 12:41:00 +0300

    Create dat_generator.py

---
+++ dat_generator.py
@@ -0,0 +1,31 @@
+from pgmpy.readwrite import BIFReader
+from pgmpy.sampling import BayesianModelSampling
+import pandas as pd
+
+# Load the .bif file
+reader = BIFReader('./data/networks/insurance.bif')
+model = reader.get_model()
+
+# Generate samples
+sampler = BayesianModelSampling(model)
+samples = sampler.forward_sample(size=100)
+
+# Create mapping from category names to indices
+mapping = {var: {state: i for i, state in enumerate(model.get_cpds(var).state_names[var])}
+           for var in model.nodes()}
+
+# Convert category names to indices
+samples = samples.replace(mapping)
+
+# Save samples to .dat file
+with open('insurance-100.dat', 'w') as f:
+    # Write variable names (0 to n)
+    f.write(' '.join(map(str, range(len(model.nodes())))) + '\n')
+    
+    # Write the number of categories
+    categories = [len(model.get_cpds(var).state_names[var]) for var in model.nodes()]
+    f.write(' '.join(map(str, categories)) + '\n')
+    
+    # Write the samples
+    for _, row in samples.iterrows():
+        f.write(' '.join(map(str, row.values.tolist())) + '\n')
