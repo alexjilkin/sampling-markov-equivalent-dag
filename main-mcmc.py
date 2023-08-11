Fix using prior directly at the local score calculation

main-mcmc.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from probabilities import get_scores, init_scores, score
 from sampling import sample
-from utils import get_next_color, plot, seed
+from utils import plot, seed
 
 
 def test_count_equivalences():
@@ -58,16 +58,16 @@ def test_count_equivalences():
     plt.show()
 
 def test_convergence():
-    score_name = 'water-1000'
+    score_name = 'asia'
     markov_prob = 0.2
     init_scores(score_name)
 
-    n = 10000
+    n = 1000
     G = ig.Graph(directed=True)
     G.add_vertices(len(get_scores()))
 
     for _ in range(2):
-        # steps, equivalence_classes = sample(G, n, True, markov_prob, False)
+        # steps, equivalence_classes = sample(G, n, True, markov_prob, True)
         # print(f'Classes visited with equivalence: {len(equivalence_classes)}, n={n}')
         # scores = [step[1] for step in steps]
         # plt.plot(np.arange(len(scores)), scores, 'b--')
@@ -77,10 +77,10 @@ def test_convergence():
         scores = [step[1] for step in steps]
         plt.plot(np.arange(len(scores)), scores, 'g--')
 
-        steps, equivalence_classes = sample(G, n)
-        print(f'Classes visited: {len(equivalence_classes)} n={n}')
-        scores = [step[1] for step in steps]
-        plt.plot(np.arange(len(scores)), scores ,  'r-')
+        # steps, equivalence_classes = sample(G, n)
+        # print(f'Classes visited: {len(equivalence_classes)} n={n}')
+        # scores = [step[1] for step in steps]
+        # plt.plot(np.arange(len(scores)), scores ,  'r-')
 
     plt.title(f'{score_name}')
     plt.show()

new_edge_reversal.py
@@ -5,8 +5,8 @@ import numpy as np
 from utils import plot
 from probabilities import P, get_local_score, get_scores
 
-def parent_set_score(Xi, pi):
-    return get_local_score(Xi, frozenset(pi))
+def parent_set_score(Xi, pa_i, n):
+    return get_local_score(Xi, frozenset(pa_i), n)
 
 # Calculates Z (18) returns 
 def get_Z1(M:ig.Graph, X) -> (int, list[set]):
@@ -44,6 +44,8 @@ def I(pa, Xj):
 
 def new_edge_reversal_move(G: ig.Graph):
     M = G.copy()
+    n = len(M.vs)
+
     if (len(M.es) < 2):
         return G, False
     
@@ -54,7 +56,7 @@ def new_edge_reversal_move(G: ig.Graph):
 
     ## Second step, sample parent set for Xi ##
     Z2_i, parent_sets = get_Z2(M_prime, Xi, Xj)
-    Q_i_p = np.array([parent_set_score(Xi, parent_set) - Z2_i for parent_set in parent_sets])
+    Q_i_p = np.array([parent_set_score(Xi, parent_set, n) - Z2_i for parent_set in parent_sets])
 
     # Normalize probability
     max_prob = np.max(Q_i_p)
@@ -68,7 +70,7 @@ def new_edge_reversal_move(G: ig.Graph):
 
     ## Third step, sample patern set pj ##
     Z1_j, parent_sets = get_Z1(M_plus, Xj)
-    Q_j_p = np.array([parent_set_score(Xj, parent_set) - Z1_j for parent_set in parent_sets])
+    Q_j_p = np.array([parent_set_score(Xj, parent_set, n) - Z1_j for parent_set in parent_sets])
     max_prob = np.max(Q_j_p)
     Q_j_p_norm = np.exp(Q_j_p - max_prob)
     Q_j_p_norm /= np.sum(Q_j_p_norm)

probabilities.py
@@ -51,11 +51,13 @@ def init_scores(name):
 def get_scores():
     return scores
 
-def get_local_score(v, pi):
-    # Adjust from 0 to 1 counting system, boston starts from 1 but child-5000 starts from 0
-   
+def get_local_score(v, pa_i, n):
+    k = len(pa_i)
+    
     try:
-        res = scores[v][pi]
+        res = scores[v][pa_i]
+        prior = np.log( 1 / binom(n, k))
+        res += prior
     except KeyError:
         res = -np.inf
 
@@ -64,16 +66,18 @@ def get_local_score(v, pi):
 def score(G: ig.Graph):
     score = 0
     n = len(G.vs)
+
     for v in G.vs:
         pi = frozenset(map(lambda x: x, G.predecessors(v)))
-        local_score = get_local_score(v.index, pi)
+        local_score = get_local_score(v.index, pi, n)
         
         # If it is inf, just return
         if (local_score == -np.inf):
             return local_score
+        
         k = len(G.predecessors(v))
-        prior = np.log( 1 / binom(n, k))
-        score += (local_score + prior)
+        
+        score += local_score 
 
     return score
     